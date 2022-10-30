128764a1838d714829dee59eb8752de131336248
DiegoUpgradeHub <diego.perez@bootcamp-upgrade.com>
2022-10-30 18:18:56 +0100

    first commit

---
+++ iteracion3.js
@@ -0,0 +1,24 @@
+//Iteración #3: Operadores
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+let multiply = 10 * 5;
+console.log('Punto 1.1: ', multiply);
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+let divide = 10 / 2;
+console.log('Punto 1.2: ', divide);
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+let remainder = 15 % 9;
+console.log('Punto 1.3: ', remainder);
+
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, teniendo dos variables y = 10 y z = 5.
+let y = 10;
+let z = 5;
+let x = y + z;
+console.log('Punto 1.4: ', x);
+
+//1.5 Usa el correcto operador de asignación que resultará en x = 50, teniendo dos variables y = 10 y z = 5.
+let yy = 10;
+let zz = 5;
+let xx = zz * yy;
+console.log('Punto 1.5: ', xx);
